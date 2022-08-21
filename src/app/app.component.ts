7552eba3ecc05ce7d4c6c1288459d4bede582e0f
Dem0P4n <ch.avendano@duocuc.cl>
2022-08-21 19:34:49 -0400

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto3';
+  resultado:number = 0;
+  numero1:number = 0;
+  numero2:number = 0;
+  operacion:string = "";
+  public sumar(){
+    this.numero1 = Math.round(Math.random() * (1000 - 1) + 1)
+    this.numero2 = Math.round(Math.random() * (1000 - 1) + 1)
+    this.resultado = this.numero1 + this.numero2
+    this.operacion = "+"
+  }
+  public restar(){
+    this.numero1 = Math.round(Math.random() * (1000 - 1) + 1)
+    this.numero2 = Math.round(Math.random() * (1000 - 1) + 1)
+    this.resultado = this.numero1 - this.numero2
+    this.operacion = "-"
+  }
+  public multiplicar(){
+    this.numero1 = Math.round(Math.random() * (1000 - 1) + 1)
+    this.numero2 = Math.round(Math.random() * (1000 - 1) + 1)
+    this.resultado = this.numero1 * this.numero2
+    this.operacion = "*"
+  }
+  public dividir(){
+    this.numero1 = Math.round(Math.random() * (1000 - 1) + 1)
+    this.numero2 = Math.round(Math.random() * (1000 - 1) + 1)
+    this.resultado = this.numero1 / this.numero2
+    this.operacion = "/"
+  }
 }
